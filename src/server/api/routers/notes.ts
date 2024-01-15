0718ca2bd10b92bc48e31ebdc7feeec69f48d494
Aleksander Kopton <kopton.olek@gmail.com>
2024-01-15 20:23:39 +0100

    deleted notesList procedures from notes router

--- src/server/api/routers/notes.ts
+++ src/server/api/routers/notes.ts
@@ -1,30 +1,38 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
-import { Prisma } from "@prisma/client";
-
-type NotesListWithNotes = Prisma.NotesListGetPayload<{
-  include: { notes: true };
-}>[];
-
-const putSharedListToEnd = (array: NotesListWithNotes) => {
-  const sharedListName = "Udostępnione";
-  const sharedList = array.find((list) => list.name === sharedListName);
-  const newLists = array.filter((list) => list.name !== sharedListName);
-
-  if (sharedList) {
-    newLists.push(sharedList);
-  }
-
-  return newLists;
-};
 
 export const notesRouter = createTRPCRouter({
-  addNote: protectedProcedure
+  saveNote: protectedProcedure
     .input(
-      z.object({ title: z.string(), text: z.string(), listId: z.string() }),
+      z.object({
+        id: z.string().optional(),
+        title: z.string(),
+        text: z.string(),
+        listId: z.string(),
+      }),
     )
     .mutation(async ({ ctx, input }) => {
-      const { listId, title, text } = input;
+      const { id, listId, title, text } = input;
+
+      if (id) {
+        const note = await ctx.db.note.update({
+          where: {
+            id: id,
+          },
+          data: {
+            title,
+            text,
+            list: {
+              connect: {
+                id: listId,
+              },
+            },
+          },
+        });
+
+        return note;
+      }
+
       const note = await ctx.db.note.create({
         data: {
           title,
@@ -36,44 +44,15 @@ export const notesRouter = createTRPCRouter({
       return note;
     }),
 
-  addList: protectedProcedure.input(z.string()).mutation(({ ctx, input }) => {
-    const userId = ctx.session.user.id;
-    const list = ctx.db.notesList.create({
-      data: {
-        name: input,
-        owner: {
-          connect: {
-            id: userId,
-          },
+  getNoteById: protectedProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      const note = await ctx.db.note.findUnique({
+        where: {
+          id: input,
         },
-      },
-    });
+      });
 
-    return list;
-  }),
-
-  getLists: protectedProcedure.query(async ({ ctx }) => {
-    const userId = ctx.session.user.id;
-
-    const lists = await ctx.db.notesList.findMany({
-      where: {
-        ownerId: userId,
-      },
-      include: { notes: true },
-    });
-
-    const newLists = putSharedListToEnd(lists);
-
-    return newLists;
-  }),
-
-  getNoteById: protectedProcedure.input(z.string()).query(({ ctx, input }) => {
-    const note = ctx.db.note.findUnique({
-      where: {
-        id: input,
-      },
-    });
-
-    return note;
-  }),
+      return note;
+    }),
 });
