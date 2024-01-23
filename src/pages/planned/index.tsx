20f4820d37a398896dd186f86c25510873ea8957
Aleksander Kopton <kopton.olek@gmail.com>
2024-01-23 23:40:08 +0100

    added method to get all events and render them in app

--- src/pages/planned/index.tsx
+++ src/pages/planned/index.tsx
@@ -1,16 +1,25 @@
+import { api } from "@/utils/api";
 import Head from "next/head";
 import Link from "next/link";
 
 export default function PlannedPage() {
+  const { data: eventsList } = api.event.getEvents.useQuery();
   return (
     <>
       <Head>
         <title>Zaplanowane</title>
       </Head>
       <main>
-        <div>
+        <div className="flex flex-col">
           Zaplanowane wydarzenia
           <Link href="/planned/add-new">Zaplanuj</Link>
+          {eventsList && (
+            <ul>
+              {eventsList.map((event) => (
+                <li key={event.id}>{event.name}</li>
+              ))}
+            </ul>
+          )}
         </div>
       </main>
     </>
