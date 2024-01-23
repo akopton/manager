fdbd13870bbb861573b231c3da30b96481d9d248
Aleksander Kopton <kopton.olek@gmail.com>
2024-01-23 23:47:00 +0100

    removed unneccessary useCallback

--- src/components/Forms/AddEventForm.tsx
+++ src/components/Forms/AddEventForm.tsx
@@ -2,15 +2,12 @@ import { useEventForm } from "@/hooks/useEventForm";
 import { Form } from "../BaseComponents/Form/Form";
 import { Input } from "../BaseComponents/Input/Input";
 import { Button } from "../BaseComponents/Button/Button";
-import { useCallback, useEffect } from "react";
+import { useCallback } from "react";
 
 export const AddEventForm = () => {
   const { state, handleValue, handleSubmit } = useEventForm();
 
-  const getDateValue = useCallback(
-    (date: Date) => date.toISOString().split("T")[0],
-    [state.date],
-  );
+  const getDateValue = (date: Date) => date.toISOString().split("T")[0];
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -18,11 +15,13 @@ export const AddEventForm = () => {
         type="text"
         value={state.name}
         onChange={(e) => handleValue("name", e.currentTarget.value)}
+        error={state.errors.name}
       />
       <Input
         type="date"
         value={getDateValue(state.date) || ""}
         onChange={(e) => handleValue("date", new Date(e.currentTarget.value))}
+        error={state.errors.date}
       />
       <Button type="submit" onClick={handleSubmit} text="Zapisz" />
     </Form>
